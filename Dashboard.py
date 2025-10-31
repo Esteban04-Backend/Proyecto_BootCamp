7709f8f81de12b943746cce1fe8a8ac72d0a2d94
EstebanJ <estebanjimenezv04@gmail.com>
2025-10-31 06:54:44 -0500

    Nombres Traducidos de Columnas  y Ordanimiento por Edad

--- Dashboard.py
+++ Dashboard.py
@@ -8,27 +8,51 @@ st.set_page_config(layout="wide", page_title='Cancer', page_icon=':bar_chart:')
 st.title('Relación del Cancer')
 st.markdown('##')
 
-#Leer el archivo .csv
+#Tratar los datos del .csv
 df=pd.read_csv('data/cancer-risk-factors.csv')
-df.drop(['Patient_ID'], axis=1, inplace=True)
-df.rename(columns={'Cancer_Type':'Tipo_de_Cancer', 'Age':'Edad','Gender':'Genero'}, inplace=True)
+#Se eliminan las columnas que no se van a usar para el dashboard
+colum_eliminar=['Patient_ID','Overall_Risk_Score','Risk_Level']
+df.drop(colum_eliminar, axis=1, inplace=True)
+#Se renombra cada una de las columnas a Esapañol
+df.rename(columns={'Cancer_Type':'Tipo_de_Cancer', 'Age':'Edad','Gender':'Genero','Smoking':'Tabaquismo(%)','Alcohol_Use':'Consumo_Alcohol(%)',
+                   'Obesity':'Obesidad(%)','Family_History':'Antecedentes_Familiares','Diet_Red_Meat':'Dieta_Carnes_Rojas(%)',
+                   'Diet_Salted_Processed':'Dieta_Salada_Procesada(%)','Fruit_Veg_Intake':'Consumo_Frutas_Verduras(%)',
+                   'Physical_Activity':'Actividad_Fisica(%)','Air_Pollution':'Contaminacion_Atmosferica(%)','Occupational_Hazards':'Riesgos_Ocupacionales(%)',
+                   'BRCA_Mutation':'Mutacion_BRCA','H_Pylori_Infection':'Infeccion_H_Pylori','Calcium_Intake':'Ingesta_Calcio(%)',
+                   'BMI':'IMC','Physical_Activity_Level':'Nivel_Actividad_Fisica(%)'}, inplace=True)
+#Se renombran los tipos de cancer por su traduccion en español
 df['Tipo_de_Cancer']=df['Tipo_de_Cancer'].replace('Prostate','Prostata').replace('Skin','Piel').replace('Lung','Pulmon').replace('Breast','Seno') 
-df['Genero']=df['Genero'].replace(0,'Femenino').replace(1,'Masculino') #Se cambio el 0 por femenino y el 1 por Masculino
+#Se ordenan los datos por la edad de los pacientes
+df.sort_values(by='Edad',ascending=True, inplace=True)
+#Se cambio el 0 y el 1 para cada tipo de columna:
+#0=femenino 1=masculino
+df['Genero']=df['Genero'].replace(0,'Femenino').replace(1,'Masculino')
+#0 =negativo y 1 =Positivo"
+df['Antecedentes_Familiares']=df['Antecedentes_Familiares'].replace(0,'Negativo').replace(1,'Positivo')
+df['Mutacion_BRCA']=df['Mutacion_BRCA'].replace(0,'Negativo').replace(1,'Positivo')
+df['Infeccion_H_Pylori']=df['Infeccion_H_Pylori'].replace(0,'Negativo').replace(1,'Positivo')
+#Para las siguientes columnas se van a tratar sus datos en porcentaje para una mayor interpretacion
+#Se selecciona un subconjunto de columnas a modificar y se realiza la operacion de multiplicar por 10 para sacar un porcentaje de 0% a 100%
+columnas_procentaje=['Tabaquismo(%)','Consumo_Alcohol(%)','Obesidad(%)','Dieta_Carnes_Rojas(%)','Dieta_Salada_Procesada(%)','Consumo_Frutas_Verduras(%)',
+                   'Actividad_Fisica(%)','Contaminacion_Atmosferica(%)','Riesgos_Ocupacionales(%)','Ingesta_Calcio(%)','Nivel_Actividad_Fisica(%)']
+df[columnas_procentaje]=df[columnas_procentaje]*10
+print('\nBase de datos Tratada')
+print(df.head())
 
 #Desarrollar y/o escribir los filtros
 columnas=st.columns(4) #Creo 3 columnas que funcionan como contenedor de los filtros
-with columnas[0]:
-    demografia=st.multiselect('Selecciones los aspectos demograficos a tener en cuenta:', options=df['Genero'].unique().tolist(), max_selections=1)
-with columnas[1]:
-    habitos=st.multiselect('Seleccione los habitos y Condiciones Ambientales a tener en cuenta:', options=df['Smoking'].unique().tolist(), default=df['Smoking'].unique().tolist())
-with columnas[2]:
-    historial_medico=st.multiselect('Seleccione el historial medico a tener en cuenta:', options=df['Family_History'].unique().tolist(), default=df['Family_History'].unique().tolist())
 with columnas[3]:
-    tipoc=st.multiselect('Seleccione el tipo de cancer:',options=df['Tipo_de_Cancer'].unique().tolist(), default=df['Tipo_de_Cancer'].unique().tolist())
+    demografia=st.multiselect('Selecciones los aspectos demograficos a tener en cuenta:', options=df['Genero'].unique().tolist(), default=df['Genero'].unique().tolist())
+with columnas[1]:
+    habitos=st.multiselect('Seleccione los habitos y Condiciones Ambientales a tener en cuenta:', options=df['Tabaquismo(%)'].unique().tolist(), default=df['Tabaquismo(%)'].unique().tolist())
+with columnas[2]:
+    historial_medico=st.multiselect('Seleccione el historial medico a tener en cuenta:', options=df['Antecedentes_Familiares'].unique().tolist(), default=df['Antecedentes_Familiares'].unique().tolist())
+with columnas[0]:
+    tipoc=st.multiselect('Seleccione el tipo de cancer:',options=df['Tipo_de_Cancer'].unique().tolist(), max_selections=1)
 
 #preguntar
-data_filtered=df.loc[(df['Genero'].isin(demografia)) & (df['Smoking'].isin(habitos)) & (df['Family_History'].isin(historial_medico)) & (df['Tipo_de_Cancer'].isin(tipoc))]
-'''
+data_filtered=df.loc[(df['Genero'].isin(demografia)) & (df['Tabaquismo(%)'].isin(habitos)) & (df['Antecedentes_Familiares'].isin(historial_medico)) & (df['Tipo_de_Cancer'].isin(tipoc))]
+
 # graficos
 
 st.subheader("Visualizaciones basadas en los filtros")
@@ -44,12 +68,12 @@ else:
     st.plotly_chart(fig1, use_container_width=True)
 
     # Gráfico 2: Historial Médico y Tipo de Cáncer (Gráfico de barras)
-    fig2 = px.histogram(data_filtered, x='Family_History', color='Tipo_de_Cancer', barmode='group',
+    fig2 = px.histogram(data_filtered, x='Antecedentes_Familiares', color='Tipo_de_Cancer', barmode='group',
                        title='Casos de Cáncer según el Historial Familiar')
     st.plotly_chart(fig2, use_container_width=True)
 
     # Gráfico 3: Hábitos (Fumar) y Tipo de Cáncer (Gráfico de barras)
-    fig3 = px.histogram(data_filtered, x='Smoking', color='Tipo_de_Cancer', barmode='group',
+    fig3 = px.histogram(data_filtered, x='Tabaquismo(%)', color='Tipo_de_Cancer', barmode='group',
                        title='Casos de Cáncer según el Hábito de Fumar')
     st.plotly_chart(fig3, use_container_width=True)
 
@@ -61,4 +85,4 @@ else:
     with col2:
         st.metric(label="Casos de Cáncer Únicos", value=data_filtered['Tipo_de_Cancer'].nunique())
     with col3:
-        st.metric(label="Edades Únicas", value=data_filtered['Edad'].nunique())'''
+        st.metric(label="Edades Únicas", value=data_filtered['Edad'].nunique())
--- limpieza_data.py
+++ limpieza_data.py
@@ -2,12 +2,33 @@ import pandas as pd
 df=pd.read_csv('data/cancer-risk-factors.csv')
 print('\nBase de Datos sin Tratar')
 print(df.head())
-df.drop(['Patient_ID'], axis=1, inplace=True)
-df.rename(columns={'Cancer_Type':'Tipo_de_Cancer', 'Age':'Edad','Gender':'Genero'}, inplace=True)
-df['Tipo_de_Cancer']=df['Tipo_de_Cancer'].replace('Prostate','Prostata').replace('Skin','Piel').replace('Lung','Pulmon').replace('Breast','Seno') 
-df['Genero']=df['Genero'].replace(0,'Femenino').replace(1,'Masculino') #Se cambio el 0 por femenino y el 1 por Masculino
+#Se eliminan las columnas que no se van a usar para el dashboard
+colum_eliminar=['Patient_ID','Overall_Risk_Score','Risk_Level']
+df.drop(colum_eliminar, axis=1, inplace=True)
+#Se renombra cada una de las columnas a Esapañol
+df.rename(columns={'Cancer_Type':'Tipo_de_Cancer', 'Age':'Edad','Gender':'Genero','Smoking':'Tabaquismo(%)','Alcohol_Use':'Consumo_Alcohol(%)',
+                   'Obesity':'Obesidad(%)','Family_History':'Antecedentes_Familiares','Diet_Red_Meat':'Dieta_Carnes_Rojas(%)',
+                   'Diet_Salted_Processed':'Dieta_Salada_Procesada(%)','Fruit_Veg_Intake':'Consumo_Frutas_Verduras(%)',
+                   'Physical_Activity':'Actividad_Fisica(%)','Air_Pollution':'Contaminacion_Atmosferica(%)','Occupational_Hazards':'Riesgos_Ocupacionales(%)',
+                   'BRCA_Mutation':'Mutacion_BRCA','H_Pylori_Infection':'Infeccion_H_Pylori','Calcium_Intake':'Ingesta_Calcio(%)',
+                   'BMI':'IMC','Physical_Activity_Level':'Nivel_Actividad_Fisica(%)'}, inplace=True)
+#Se renombran los tipos de cancer por su traduccion en español
+df['Tipo_de_Cancer']=df['Tipo_de_Cancer'].replace('Prostate','Prostata').replace('Skin','Piel').replace('Lung','Pulmon').replace('Breast','Seno')
+#Se ordenan los datos por la edad de los pacientes
+df.sort_values(by='Edad',ascending=True, inplace=True)
+#Se cambio el 0 y el 1 para cada tipo de columna:
+#0=femenino 1=masculino
+df['Genero']=df['Genero'].replace(0,'Femenino').replace(1,'Masculino')
+#0 =negativo y 1 =Positivo"
+df['Antecedentes_Familiares']=df['Antecedentes_Familiares'].replace(0,'Negativo').replace(1,'Positivo')
+df['Mutacion_BRCA']=df['Mutacion_BRCA'].replace(0,'Negativo').replace(1,'Positivo')
+df['Infeccion_H_Pylori']=df['Infeccion_H_Pylori'].replace(0,'Negativo').replace(1,'Positivo')
+#Para las siguientes columnas se van a tratar sus datos en porcentaje para una mayor interpretacion
+#Se selecciona un subconjunto de columnas a modificar y se realiza la operacion de multiplicar por 10 para sacar un porcentaje de 0% a 100%
+columnas_procentaje=['Tabaquismo(%)','Consumo_Alcohol(%)','Obesidad(%)','Dieta_Carnes_Rojas(%)','Dieta_Salada_Procesada(%)','Consumo_Frutas_Verduras(%)',
+                   'Actividad_Fisica(%)','Contaminacion_Atmosferica(%)','Riesgos_Ocupacionales(%)','Ingesta_Calcio(%)','Nivel_Actividad_Fisica(%)']
+df[columnas_procentaje]=df[columnas_procentaje]*10
 print('\nBase de datos Tratada')
 print(df.head())
-#Contar segun edad y Genero
-grupos=df.groupby(['Genero'])['Edad'].count()
-print(grupos)
+#df.to_excel('Cancer.xlsx', index=False)
+print('Se creo el archivo Excel Correctamente')
